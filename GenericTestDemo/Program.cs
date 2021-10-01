55b172fdd589a05f553a9e6fb1ca032a3a6e7b7a
Mabel Christina <maybelchristina@gmail.com>
2021-10-01 18:47:29 +0530

    [Mabel] Add:UC1 added

---
+++ GenericTestDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericTestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxOfNumber maxOfNumber = new MaxOfNumber();
+            Console.WriteLine(maxOfNumber.Max(10, 2, 1));
+        }
+    }
+}
